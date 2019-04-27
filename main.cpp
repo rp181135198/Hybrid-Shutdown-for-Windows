105fc99f4f0c263d4254ffb7a697870fe42ce90c
Ramesh Prasad <49446878+rp181135198@users.noreply.github.com>
2019-04-27 11:08:39 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include<stdlib.h>
 int main()
 {
-    
+    system("Hybrid-Shutdown-for-Windows");
+    system("echo ATTENTION");
+    system("echo This will shutdown your Computer and prepare it for fast startup.");
+    system("Do you want to Proceed? (Y/N) : ");
     return 0;
 }
